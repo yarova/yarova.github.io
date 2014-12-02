918fc3bb5acb60e04adab33da966a453c8ccf626
Kateryna Yarova <katesun18@gmail.com>
2014-12-02 23:52:47 +0100

    test test test

--- test2/js/scripts.js
+++ test2/js/scripts.js
@@ -1,10 +1,14 @@
 $(function(){
 
+    $.fn.scrollTo = function(duration, easing, complete){
+        return $.each(this, function(){
+            $('html, body').animate({ scrollTop: $(this).offset().top }, duration, easing, complete);
+        })
+    };
 
-
-
-
-
-
+    $(document).on('click', 'a[href^=#]:not([role="button"])', function(e){
+        e.preventDefault();
+        $($(this).attr('href')).scrollTo(600, 'swing');
+    });
 
 });
