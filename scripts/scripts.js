347afdcf5b5f9f48ad6130601decea4f1c76435f
Kateryna Yarova <katesun18@gmail.com>
2015-07-04 12:46:24 +0300

    Add google analytics script

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,5 +1,17 @@
 $(function(){
 
+
+
+    /*(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-64772694-1', 'auto');
+    ga('send', 'pageview');*/
+
+
+
     $(window).on('resize', function(){
         $('#landing-page svg').css('height', Math.floor(window.outerWidth * 0.1));
     });
